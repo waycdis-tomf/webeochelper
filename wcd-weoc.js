db882d301192c53a634202970fe755235463f730
waycdis-tomf <tom.flicker@waycdis.com>
2026-02-17 16:13:41 -0500

    Added pdf property to wcd.

--- wcd-weoc.js
+++ wcd-weoc.js
@@ -148,6 +148,7 @@ class wcdLibrary {
 
         // Sets various parameters based on the current WebEOC record to be used
         this.dataid = queryParams.dataid;
+        this.pdf = queryParams.pdf;
         this.relateddataid = queryParams.relateddataid;
         this.viewid = queryParams.viewid;
         this.tableid = queryParams.tableid;
