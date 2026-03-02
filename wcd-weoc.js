43752c62f1c00f9773d6cd7f28318259d3629008
Jaime Ovalle <jaime.ovalle@waycdis.com>
2026-03-02 17:01:56 -0600

    troubleshooting 13

--- wcd-weoc.js
+++ wcd-weoc.js
@@ -415,7 +415,7 @@ class wcdLibrary {
                 } else if (formElement.classList.contains('wcd-select')) {
                     nextSibling = formElement.parentNode.parentNode.parentNode.nextSibling;
                 }
-                if ((!!nextSibling) && (nextSibling instanceof HTMLDivElement || nextSibling instanceof HTMLDivElement === false) && nextSibling.classList.contains('invalid-feedback')) {
+                if ((!!nextSibling) && /*(nextSibling instanceof HTMLDivElement || nextSibling instanceof HTMLDivElement === false) */ && nextSibling.classList.contains('invalid-feedback')) {
                     console.log('nextSibling', nextSibling)
                     nextSibling.style.display = "block";
                 }
